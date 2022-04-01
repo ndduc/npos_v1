dcfc08da843bf688174d5cc3c84edc51f5502277
ndduc <ndduc1856@gmail.com>
2022-03-31 19:32:46 -0700

    update const string ui value

--- lib/Constant/UI/uiItemList.dart
+++ lib/Constant/UI/uiItemList.dart
@@ -6,25 +6,25 @@ List<Map<dynamic, dynamic>> menuItem = [
   {"id" : 2, "name" : "Section/Aisle", "event" : "MENU_SEC_MAN"},
   {"id" : 3, "name" : "Discount & Tax", "event" : "MENU_DIS/TAX_MAN"},
   {"id" : 4, "name" : "Vendor/Supplier", "event" : "MENU_VEN_MAN"},
-  {"id" : 5, "name" : "Employee Management", "event" : "MENU_EMP_MAN"},
-  {"id" : 6, "name" : "Store/Location Management", "event" : "MENU_LOC_MAN"},
-  {"id" : 7, "name" : "System Setup", "event":"MENU_SETUP"},
+  {"id" : 5, "name" : "Employee Management\n(Not Done)", "event" : "MENU_EMP_MAN"},
+  {"id" : 6, "name" : "Store/Location Management\n(To Be Implemented)", "event" : "MENU_LOC_MAN"},
+  {"id" : 7, "name" : "System Setup\n(To Be Implemented)", "event":"MENU_SETUP"},
 
-  {"id" : 8, "name" : "Inventory Report", "event" : "MENU_INV_REPORT"},
-  {"id" : 9, "name" : "Detail Report", "event" : "MENU_DE_REPORT"},
-  {"id" : 10, "name" : "Daily Report", "event" : "MENU_DAI_REPORT"},
-  {"id" : 11, "name" : "Analysis/Cashier Performance Report", "event" : "MENU_ANA_REPORT"},
-  {"id" : 12, "name" : "Customer Royalty", "event" : "MENU_CUS_ROYAL"},
-  {"id" : 13, "name" : "Point Management", "event" : "MENU_POINT_MAN"},
-  {"id" : 14, "name" : "Customer Notification", "event" : "MENU_CUS_NOTI"},
-  {"id" : 15, "name" : "Social Media Management", "event" : "MENU_SOC_MAN"},
+  {"id" : 8, "name" : "Inventory Report\n(To Be Implemented)", "event" : "MENU_INV_REPORT"},
+  {"id" : 9, "name" : "Detail Report\n(To Be Implemented)", "event" : "MENU_DE_REPORT"},
+  {"id" : 10, "name" : "Daily Report\n(To Be Implemented)", "event" : "MENU_DAI_REPORT"},
+  {"id" : 11, "name" : "Analysis/Cashier Performance Report\n(To Be Implemented)", "event" : "MENU_ANA_REPORT"},
+  {"id" : 12, "name" : "Customer Royalty\n(To Be Implemented)", "event" : "MENU_CUS_ROYAL"},
+  {"id" : 13, "name" : "Point Management\n(To Be Implemented)", "event" : "MENU_POINT_MAN"},
+  {"id" : 14, "name" : "Customer Notification\n(To Be Implemented)", "event" : "MENU_CUS_NOTI"},
+  {"id" : 15, "name" : "Social Media Management\n(To Be Implemented)", "event" : "MENU_SOC_MAN"},
 
 ];
 
 List<Map<dynamic, dynamic>> menuItemLeft = [
-  {"id" : 0, "name" : "POS Client", "event" : "PC"},
-  {"id" : 1, "name" : "End Of Day/Close Store", "event" : "CS"},
-  {"id" : 2, "name" : "Logout", "event" : "LO"},
+  {"id" : 0, "name" : "POS Client\n(Implementing)", "event" : "PC"},
+  {"id" : 1, "name" : "End Of Day/Close Store\n(To Be Implemented)", "event" : "CS"},
+  {"id" : 2, "name" : "Logout\n(To Be Implemented)", "event" : "LO"},
 ];
 
 List<Map<dynamic, dynamic>> menuItemLeftNested = [
